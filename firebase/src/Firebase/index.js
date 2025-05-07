3977df01c43f44f975e69f8d8c039a44af0914df
CarsonChan07 <carson.chan@student.tdsb.on.ca>
2025-05-07 15:10:27 -0400

    Imported Firebase authentication and firestore

--- firebase/src/Firebase/index.js
+++ firebase/src/Firebase/index.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB1o_iznf7LHNyEdX9Wi74XtCbnm-MAhss",
   authDomain: "ics4u-ccc14.firebaseapp.com",
@@ -13,7 +11,6 @@ const firebaseConfig = {
   appId: "1:708282590973:web:72d41ad3d19ffe5f2a0094"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseConfig);
 const firestore = getFirestore(firebaseConfig);
